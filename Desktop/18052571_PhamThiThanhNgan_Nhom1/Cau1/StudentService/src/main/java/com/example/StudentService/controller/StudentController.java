3f5a1fa8ff4561bb63f62153f35a4238dbf35202
pham.thanhngan___ <phamthithanhngan@MacBook-Pro.local>
2022-11-27 16:12:07 +0700

    finish retry to call passenger service

--- Desktop/18052571_PhamThiThanhNgan_Nhom1/Cau1/StudentService/src/main/java/com/example/StudentService/controller/StudentController.java
+++ Desktop/18052571_PhamThiThanhNgan_Nhom1/Cau1/StudentService/src/main/java/com/example/StudentService/controller/StudentController.java
@@ -1,6 +1,7 @@
 package com.example.StudentService.controller;
 
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,8 @@ import com.example.StudentService.dto.StudentDTO;
 import com.example.StudentService.entity.Student;
 import com.example.StudentService.service.StudentService;
 
+import io.github.resilience4j.retry.annotation.Retry;
+
 @RequestMapping("/student")
 @RestController
 public class StudentController {
@@ -23,12 +26,20 @@ public class StudentController {
 	
 	
 	@GetMapping("/{id}")
-//	@Retry(name="customer")
+	@Retry(name="customer")
 	public StudentDTO getOne(@PathVariable String id) {
   
-	    
+		try {
+	        System.out.println(" time wait retry  "+
+	                LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")));
+        
+	    } catch (Exception e){
+	        System.out.println(e.getLocalizedMessage());
+	    }
 		return service.getStudentDTO(id);
 	}
+		
+	
 	//cau 1c
 	@PostMapping()
 	public Student addBill(@RequestBody Student o) {
